936ae9e8139dc9cfe6702e888f5f6187efd8b08b
Roque Alejandro Gonzalez Candia <alejandrogcandia@gmail.com>
2023-07-03 21:10:30 -0300

    se crea loader

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,30 @@ import Navbar from './components/Navbar/Navbar.jsx'
 import Footer from "./components/Footer/Footer.jsx"
 import {Outlet} from 'react-router-dom'
 import styles from './App.module.css'
+import { useEffect, useState } from 'react';
+import Loader from "./components/Loader/Loader.jsx"
 function App() {
+  const [load,setLoad] = useState(true)
+  useEffect(()=>{
+    window.addEventListener('load', function() {
+      setTimeout(function() {
+        // Código a ejecutar después del tiempo especificado
+        setLoad(false)
+      }, 1500); // 3000 milisegundos (3 segundos)
+    
+    });
+  },[])
 
   return (
     <Container 
     maxWidth='xl'
     className={styles.container}
     >
+     
+
+     <Loader load={load} /> 
+     
+      
      <Navbar/> 
      <Outlet/>
       <Footer/>
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import styles from './Loader.module.css'
+export default function Loader({load}) {
+  return (
+   <div className={`${load ? styles.loaderOn : styles.loaderOff}`}>
+    <Box className={styles.spiner}>
+     <CircularProgress />
+   </Box>
+   </div>
+    
+  );
+}
+
