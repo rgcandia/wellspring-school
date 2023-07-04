45b2260ac26ee4e4cb229f0f75ac66ccd488f8f4
Ale Candia <alejandrogcandia@gmail.com>
2023-07-04 13:37:22 -0300

    prueba imagen responsive del index

--- src/components/Index/Index.jsx
+++ src/components/Index/Index.jsx
@@ -1,7 +1,15 @@
+import styles from './Index.module.css'
+import image from '../../images/aerea_ws.jpg'
 export default function Index (){
-    return(<>
+    return(<div className={styles.container}>
 
-<h1>Index</h1>
+<div className={styles.box1}>
+    <img className={styles.image}  alt={image} src={image}/>
+</div>
+<div className={styles.box2}>INSTITUCIONAL</div>
+<div className={styles.box3}>box3</div>
+<div className={styles.box4}>box3</div>
+<div className={styles.box5}>box4</div>
 
-</>)
+</div>)
 }
--- src/components/Index/Institucional/Institucional.jsx
+++
@@ -1,6 +0,0 @@
-export default function Institucional (){
-    return(<>
-
-<h1>Institucional</h1>
-</>)
-}
