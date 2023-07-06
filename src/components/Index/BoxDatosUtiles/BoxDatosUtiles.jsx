c843de8e285022786e4b02a9aa7e09050d965416
Ale Candia <alejandrogcandia@gmail.com>
2023-07-06 11:51:56 -0300

    index - box datos utiles ok

--- src/components/Index/BoxDatosUtiles/BoxDatosUtiles.jsx
+++ src/components/Index/BoxDatosUtiles/BoxDatosUtiles.jsx
@@ -3,15 +3,15 @@ import {getIcons} from './services.js'
 import styles from './BoxDatosUtiles.module.css'
 export default function BoxDatosUtiles (){
     return(<Box className={styles.main} sx={{backgroundColor:'primary.main'}}>
-        <Box>
+        <Box className={styles.title}>
 
-        <Typography variant='h5' sx={{color:'secondary.main',textAlign:'center',fontStyle:'italic', padding:'5px'}} >DATOS UTILES</Typography>
+        <Typography variant='h5' component='p' sx={{color:'secondary.main',textAlign:'center',fontStyle:'italic', padding:'5px'}} >DATOS UTILES</Typography>
         </Box>
          <Box className={styles.container}>
             {getIcons.map(e=>{
                 return(<Box className={styles.box} key={e.path} >
                     <Box className={styles.imageContainer} ><img className={styles.image}  alt={e.path} src={e.path}/></Box>
-                    <Typography  variant='overline' sx={{color:'secondary.main',fontWeight:'200'}} className={styles.label}>{e.label}</Typography>
+                    <Typography component='p' variant='overline' sx={{color:'secondary.main',fontWeight:'200'}} className={styles.label}>{e.label}</Typography>
                 </Box>)
             })}
          </Box>
