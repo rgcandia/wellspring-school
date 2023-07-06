b2f4aec69f172c51f235d650476263c828e0fb93
Ale Candia <alejandrogcandia@gmail.com>
2023-07-06 11:17:25 -0300

    config componente BoxDatosUtiles

--- src/components/Index/BoxDatosUtiles/BoxDatosUtiles.jsx
+++ src/components/Index/BoxDatosUtiles/BoxDatosUtiles.jsx
@@ -3,7 +3,10 @@ import {getIcons} from './services.js'
 import styles from './BoxDatosUtiles.module.css'
 export default function BoxDatosUtiles (){
     return(<Box className={styles.main} sx={{backgroundColor:'primary.main'}}>
+        <Box>
+
         <Typography variant='h5' sx={{color:'secondary.main',textAlign:'center',fontStyle:'italic', padding:'5px'}} >DATOS UTILES</Typography>
+        </Box>
          <Box className={styles.container}>
             {getIcons.map(e=>{
                 return(<Box className={styles.box} key={e.path} >
