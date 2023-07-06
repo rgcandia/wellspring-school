ce833f38c21f6ea75765e2afbf4d808fcf1571b4
Ale Candia <alejandrogcandia@gmail.com>
2023-07-06 10:32:44 -0300

    boxDatosUtiles responsive

--- src/components/Index/BoxDatosUtiles/BoxDatosUtiles.jsx
+++ src/components/Index/BoxDatosUtiles/BoxDatosUtiles.jsx
@@ -2,7 +2,7 @@ import { Box, Typography } from "@mui/material";
 import {getIcons} from './services.js'
 import styles from './BoxDatosUtiles.module.css'
 export default function BoxDatosUtiles (){
-    return(<Box  sx={{backgroundColor:'primary.main'}}>
+    return(<Box className={styles.main} sx={{backgroundColor:'primary.main'}}>
         <Typography variant='h5' sx={{color:'secondary.main',textAlign:'center',fontStyle:'italic', padding:'5px'}} >DATOS UTILES</Typography>
          <Box className={styles.container}>
             {getIcons.map(e=>{
