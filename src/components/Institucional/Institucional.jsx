116ec8cab8ceeda8313b0b7c7a66f95911950d23
Roque Alejandro Gonzalez Candia <alejandrogcandia@gmail.com>
2023-07-11 10:09:09 -0300

    se trabaja en el componente Institucional

--- src/components/Institucional/Institucional.jsx
+++ src/components/Institucional/Institucional.jsx
@@ -1,8 +1,11 @@
 import { Box, Button } from '@mui/material'
 import Propuesta from './Propuesta/Propuesta.jsx'
 import Orientacion from './Orientacion/Orientacion.jsx'
+import Texto from './Texto/Texto.jsx'
 import { useState } from 'react'
+import background from '../../images/institucional/institucional_background.jpg'
 import styles from './Institucional.module.css'
+
 export default function Institucional (){
     const [orientacion,setOrientacion] =  useState(false)
     const [propuesta,setPropuesta] = useState(true)
@@ -14,11 +17,17 @@ export default function Institucional (){
         setPropuesta(!propuesta)
         setOrientacion(!orientacion)
     }
-    return(<Box>
+    return(<Box className={styles.institucional}>
 
-<Box className={styles.image}>IMAGEN DE FONDO</Box>
+<Box className={styles.image}>
+  <img
+  src={background}
+  alt='background cabecera institucional'
+  />
+ 
+</Box>
 <Box className={styles.text}>
-    tEXTO
+    <Texto/>
 </Box>
 <Box className={styles.container}>
     <Box>
---
+++ src/components/Institucional/Texto/Texto.jsx
@@ -0,0 +1,34 @@
+import { Box, Typography } from "@mui/material";
+
+export default function Texto (){
+    return(<>
+    <Typography variant='h2' textAlign='center'>INSTITUCIONAL</Typography>
+
+
+        <Box>
+        <Typography variant='h5'>IDENTIDAD</Typography>
+        <Typography>
+            Wellspring School ofrece a sus alumnos, sin distincion de raza o creencias una educación laica, mixta y 
+            bilingûe basada en principios didácticos y una metodología activa personalizada que apunta a una formación integral
+        </Typography>
+        </Box>
+        
+         <Box>
+            <Typography variant='h5'>NUESTRO COMIENZO</Typography>
+            <Typography>
+                Fundado en el año 1992, nuestro Colegio ha sido diseñado y construido contemplando todas las necesidades de bienestar del alumno, en un entorno
+                pensado para fomentar el acercamiento con la naturaleza.
+                Los distintos sectores se han distribuido dentro de las 4 hectáreas del campus buscando que el asentamiento respetara el plácido entorno natural,
+                evitando la centralización pero al mismo tiempo, contemplando en cada área las necesidades requeridas para su función específica.
+            </Typography>
+            
+            </Box>   
+
+            <Box>
+
+
+            </Box>
+
+       
+    </>)
+}
