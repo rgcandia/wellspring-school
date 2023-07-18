f7d087b2d51f4470a88d6366b858fc16ad52996f
Ale Candia <alejandrogcandia@gmail.com>
2023-07-18 12:32:14 -0300

    configuracion del formulñario dinamico

--- src/components/Dashboard/Unlog/Unlog.jsx
+++ src/components/Dashboard/Unlog/Unlog.jsx
@@ -2,13 +2,14 @@ import { Box, Button } from "@mui/material";
 import {disconect} from '../../../firebase/auth_sign_out.js'
 import { useDispatch } from "react-redux";
 import {uploadUser} from '../../../redux/slice.js'
+import styles from './Unlog.module.css'
 export default function Unlog(){
     const dispatch = useDispatch;
   const handleClick = ()=>{
     disconect(dispatch,uploadUser);
     location.reload()
   }
-    return(<Box>
+    return(<Box className={styles.unlog}>
         <Button variant='outlined' color='primary' onClick={handleClick}>
        Cerrar sesion
         </Button>
