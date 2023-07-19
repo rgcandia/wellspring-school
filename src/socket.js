c64b0ff3c4b6f8110fc6a7ee34afdd7e08d7af1a
Ale Candia <alejandrogcandia@gmail.com>
2023-07-19 09:11:38 -0300

    config io socket deploy

--- src/socket.js
+++ src/socket.js
@@ -3,7 +3,7 @@ let socket;
 
 // inicia socket con el usuario en el evento join
 export const initSocket = (user)=>{
-    socket = io('http://localhost:4001/',{transports:['websocket']})
+    socket = io('https://wellspring-api.vercel.app/',{transports:['websocket']})
     console.log('Connecting socket...')
     if (socket && user) {
         socket.emit("join", user);
