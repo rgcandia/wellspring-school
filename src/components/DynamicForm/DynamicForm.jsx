ed82c6e48b0a5592ada48b662192ba008c7ae7d0
Ale Candia <alejandrogcandia@gmail.com>
2023-07-20 12:55:18 -0300

    configuracion de sewwtaler2 en el envio del form

--- src/components/DynamicForm/DynamicForm.jsx
+++ src/components/DynamicForm/DynamicForm.jsx
@@ -1,18 +1,19 @@
 import { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, Navigate, useNavigate } from "react-router-dom";
 import { Box, Button, Typography } from "@mui/material";
 import ItemForm from "./ItemForm/ItemForm";
 import {updateForm} from '../../socket.js'
-
+import {alertSendFormOk} from '../../services.js'
 import styles from "./DynamicForm.module.css";
 
 export default function DynamicForm() {
+  const navigate = useNavigate();
   const formRef = useRef(null);
   const { id } = useParams();
   const { values,user } = useSelector((state) => state.value);
   const data = values?.find((item) => item.id == id);
- const {model} = data;
+ const model = data?.model;
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -31,7 +32,15 @@ export default function DynamicForm() {
     // Ejecutar fetch a la API con el formulario en el body
     // ...
     updateForm({id,form:arrayForm,user})
+    alertSendFormOk()
+    .then((result)=>{
+      if(result.isConfirmed){
+        navigate('/dashboard')
+      }
+    })
+    
   };
+
 
 
   return (
--- src/services.js
+++ src/services.js
@@ -1,7 +1,25 @@
 import {authListener} from './firebase/auth_state_listener.js'
 import {uploadUser} from './redux/slice.js'
+import Swal from 'sweetalert2'
+
+
 
 export function initListener(dispatch){
  authListener(dispatch,uploadUser)
  
+}
+export function alertSendFormOk(){
+     
+
+  return   Swal.fire(
+    {
+        title:'Buen trabajo !',
+        text:'Formulario enviado correctamente',
+        icon:'success',
+        confirmButtonColor:'#0c3f5c',
+       
+
+    }
+    
+  )
 }
