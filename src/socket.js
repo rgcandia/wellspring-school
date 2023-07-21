12899dc5b773efcb8f461a650fa5ee8a6416a2b1
Ale Candia <alejandrogcandia@gmail.com>
2023-07-21 10:40:47 -0300

    reconfig deploy

--- src/socket.js
+++ src/socket.js
@@ -3,8 +3,8 @@ let socket;
 
 // inicia socket con el usuario en el evento join
 export const initSocket = (user)=>{
-    socket = io('https://wellspring-api.adaptable.app/',{transports:['websocket']})
-    //socket = io('http://localhost:4001/',{transports:['websocket']})
+    //socket = io('https://wellspring-api.adaptable.app/',{transports:['websocket']})
+    socket = io('http://localhost:4001/',{transports:['websocket']})
     console.log('Connecting socket...')
     if (socket && user) {
         socket.emit("join", user);
