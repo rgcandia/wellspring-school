a9a1aef8b82e39d925a0831cc490371aca7cc3af
Ale Candia <alejandrogcandia@gmail.com>
2023-08-03 13:31:29 -0300

    se trababaja en formulario componente TeatroForm

--- src/components/DynamicForm/Body/Form/RenderForm/TeatroForm/TeatroForm.jsx
+++ src/components/DynamicForm/Body/Form/RenderForm/TeatroForm/TeatroForm.jsx
@@ -35,10 +35,19 @@ dispatch(updateForm({...formData,teatro:{...formData.teatro,dataSobreEscenario:{
     dispatch(updateForm({...formData,teatro:{...formData.teatro,dataSobreEscenario:{...formData.teatro.dataSobreEscenario,[id]:value}}}))
   }
 
-  const handleCheckChangeBajoEscenario = (e)=>{
+  const handleCheckChangeSobreEscenario = (e)=>{
     const {name, checked} = e.target;
     dispatch(updateForm({...formData,teatro:{...formData.teatro,dataSobreEscenario:{...formData.teatro.dataSobreEscenario,[name]:checked}}}))
   }  
+  const handleChangeBajoEscenario =(e)=>{
+    const {value,id} = e.target;
+    dispatch(updateForm({...formData,teatro:{...formData.teatro,dataBajoEscenario:{...formData.teatro.dataBajoEscenario,[id]:value}}}))
+  }
+  const handleCheckChangeBajoEscenario = (e)=>{
+    const {name, checked} = e.target;
+    dispatch(updateForm({...formData,teatro:{...formData.teatro,dataBajoEscenario:{...formData.teatro.dataBajoEscenario,[name]:checked}}}))
+  }  
+
   return (<Box>
   <Box >
          <Typography><strong>Sección Teatro</strong></Typography>
@@ -155,6 +164,14 @@ dispatch(updateForm({...formData,teatro:{...formData.teatro,dataSobreEscenario:{
          fullWidth
          onChange={handleChangeSobreEscenario}
          />
+             <FormControlLabel
+      label='Pie de micrófono'
+      control={<Checkbox
+      checked={formData?.teatro.dataSobreEscenario.pieMicrofono || false}
+      name='pieMicrofono'
+      onChange={handleCheckChangeSobreEscenario}
+      />}
+    />
           <TextField
          id='lucesYPulsadores'
          type='number'
@@ -169,20 +186,13 @@ dispatch(updateForm({...formData,teatro:{...formData.teatro,dataSobreEscenario:{
          fullWidth
          onChange={handleChangeSobreEscenario}
          />
-          <FormControlLabel
-      label='Pie de micrófono'
-      control={<Checkbox
-      checked={formData?.teatro.dataSobreEscenario.pieMicrofono || false}
-      name='pieMicrofono'
-      onChange={handleCheckChangeBajoEscenario}
-      />}
-    />
+      
           <FormControlLabel
       label='Proyector sobre escenario'
       control={<Checkbox
       checked={formData?.teatro.dataSobreEscenario.proyector || false}
       name='proyector'
-      onChange={handleCheckChangeBajoEscenario}
+      onChange={handleCheckChangeSobreEscenario}
       />}
     />
         <FormControlLabel
@@ -190,7 +200,7 @@ dispatch(updateForm({...formData,teatro:{...formData.teatro,dataSobreEscenario:{
       control={<Checkbox
       checked={formData?.teatro.dataSobreEscenario.puntero || false}
       name='puntero'
-      onChange={handleCheckChangeBajoEscenario}
+      onChange={handleCheckChangeSobreEscenario}
       />}
     />
  
@@ -199,7 +209,7 @@ dispatch(updateForm({...formData,teatro:{...formData.teatro,dataSobreEscenario:{
       control={<Checkbox
       checked={formData?.teatro.dataSobreEscenario.pantalla || false}
       name='pantalla'
-      onChange={handleCheckChangeBajoEscenario}
+      onChange={handleCheckChangeSobreEscenario}
       />}
     />
      <FormControlLabel
@@ -207,7 +217,7 @@ dispatch(updateForm({...formData,teatro:{...formData.teatro,dataSobreEscenario:{
       control={<Checkbox
       checked={formData?.teatro.dataSobreEscenario.computadora || false}
       name='computadora'
-      onChange={handleCheckChangeBajoEscenario}
+      onChange={handleCheckChangeSobreEscenario}
       />}
     />
     
@@ -224,6 +234,110 @@ dispatch(updateForm({...formData,teatro:{...formData.teatro,dataSobreEscenario:{
       onChange={handleCheckChange}
       />}
     />
+    {formData.teatro.bajoEscenario &&
+    <Box sx={{display:'flex',flexDirection:'column',gap:'10px'}}>
+       <TextField
+         id='pupitres'
+         type='number'
+         label='Pupitres (cantidad)'
+         fullWidth
+         onChange={handleChangeBajoEscenario}
+         />
+            <TextField
+         id='manteles'
+         type='number'
+         label='Manteles (cantidad)'
+         fullWidth
+         onChange={handleChangeBajoEscenario}
+         />
+           <TextField
+         id='sillas'
+         type='number'
+         label='Sillas (cantidad)'
+         fullWidth
+         onChange={handleChangeBajoEscenario}
+         />
+            <TextField
+         id='gradas'
+         type='number'
+         label='Gradas (cantidad)'
+         fullWidth
+         onChange={handleChangeBajoEscenario}
+         />
+            <TextField
+         id='tarimas'
+         type='number'
+         label='Tarimas (cantidad)'
+         fullWidth
+         onChange={handleChangeBajoEscenario}
+         />
+            <TextField
+         id='microfonoInalambrico'
+         type='number'
+         label='Micrófono Inalambrico (cantidad)'
+         fullWidth
+         onChange={handleChangeBajoEscenario}
+         />
+          <FormControlLabel
+      label='Proyector Arriba'
+      control={<Checkbox
+      checked={formData?.teatro.dataBajoEscenario.proyector || false}
+      name='proyector'
+      onChange={handleCheckChangeBajoEscenario}
+      />}
+    />
+
+<FormControlLabel
+      label='Fondo de prensa'
+      control={<Checkbox
+      checked={formData?.teatro.dataBajoEscenario.fondoPrensa || false}
+      name='fondoPrensa'
+      onChange={handleCheckChangeBajoEscenario}
+      />}
+    />
+    <FormControlLabel
+      label='Computadora'
+      control={<Checkbox
+      checked={formData?.teatro.dataBajoEscenario.computadora || false}
+      name='computadora'
+      onChange={handleCheckChangeBajoEscenario}
+      />}
+    />
+    <FormControlLabel
+      label='Alargue'
+      control={<Checkbox
+      checked={formData?.teatro.dataBajoEscenario.alargue || false}
+      name='alargue'
+      onChange={handleCheckChangeBajoEscenario}
+      />}
+    />
+    <FormControlLabel
+      label='Aire Acondicionado'
+      control={<Checkbox
+      checked={formData?.teatro.dataBajoEscenario.aireAcondicionado || false}
+      name='aireAcondicionado'
+      onChange={handleCheckChangeBajoEscenario}
+      />}
+    />
+    <FormControlLabel
+      label='Café'
+      control={<Checkbox
+      checked={formData?.teatro.dataBajoEscenario.cafe || false}
+      name='cafe'
+      onChange={handleCheckChangeBajoEscenario}
+      />}
+    />
+    <FormControlLabel
+      label='Agua caliente'
+      control={<Checkbox
+      checked={formData?.teatro.dataBajoEscenario.aguaCaliente || false}
+      name='aguaCaliente'
+      onChange={handleCheckChangeBajoEscenario}
+      />}
+    />
+    </Box>
+    }
+    
     </Box>
 
     </Box>
