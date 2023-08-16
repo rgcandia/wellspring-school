508ece34248d9cfa1d2411a211a1090aa2e676ed
Ale Candia <alejandrogcandia@gmail.com>
2023-08-16 14:13:12 -0300

    se trabaja en el componente outlog, se cambia el icono del button, y se agregsa el nombre del user

--- src/components/Dashboard/Unlog/Unlog.jsx
+++ src/components/Dashboard/Unlog/Unlog.jsx
@@ -1,19 +1,31 @@
 import { Box, Button, Typography } from "@mui/material";
 import {disconect} from '../../../firebase/auth_sign_out.js'
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {uploadUser} from '../../../redux/slice.js'
+import LogoutIcon from '@mui/icons-material/Logout';
 import styles from './Unlog.module.css'
 export default function Unlog(){
+  const {user} = useSelector(state=>state.data)
     const dispatch = useDispatch;
   const handleClick = ()=>{
     disconect(dispatch,uploadUser);
     location.reload()
   }
     return(<Box className={styles.unlog}>
-        <Button variant='outlined' color='primary' onClick={handleClick}>
-       Cerrar sesion
+       
+       
+       <Box sx={{display:'flex',alignItems:'center',justifyContent:'center'}}>
+        <Typography variant='body2'>Bienvenido : <strong>{user}</strong></Typography>
+       </Box>
+      
+       <Box sx={{display:'flex',alignItems:'center',justifyContent:'center'}}>
+       <Button variant='outlined' color='primary' onClick={handleClick}>
+       <LogoutIcon/>
+       <Typography>logout</Typography>
         </Button>
-        
+       </Box>
+      
+       
      
     </Box>)
 }
