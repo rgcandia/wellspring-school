0a3284ea8404fd76d76e00666072464bd81e228a
Ale Candia <alejandrogcandia@gmail.com>
2023-08-17 13:15:04 -0300

    se realiza un modal para el componente Viajes de Estudio

--- src/components/ViajesEstudio/Imagenes/Imagenes.jsx
+++ src/components/ViajesEstudio/Imagenes/Imagenes.jsx
@@ -1,54 +1,55 @@
-import { Box } from "@mui/material"
-import imagen1 from '../../../images/Viajes/viaje1.jpg'
-import imagen2 from '../../../images/Viajes/viaje2.jpg'
-import imagen3 from '../../../images/Viajes/viaje3.jpg'
-import imagen4 from '../../../images/Viajes/viaje4.jpg'
-import imagen5 from '../../../images/Viajes/viaje5.jpg'
-import imagen6 from '../../../images/Viajes/viaje6.jpg'
-import styles from './Imagenes.module.css'
-export default function Imagenes (){
-    return(<Box className={styles.imagenes}>
-        <Box>
+import React, { useState } from "react";
+import { Box, Modal } from "@mui/material";
+import imagen1 from '../../../images/Viajes/viaje1.jpg';
+import imagen2 from '../../../images/Viajes/viaje2.jpg';
+import imagen3 from '../../../images/Viajes/viaje3.jpg';
+import imagen4 from '../../../images/Viajes/viaje4.jpg';
+import imagen5 from '../../../images/Viajes/viaje5.jpg';
+import imagen6 from '../../../images/Viajes/viaje6.jpg';
+import styles from './Imagenes.module.css';
+
+export default function Imagenes() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageClick = (imageSrc) => {
+    setSelectedImage(imageSrc);
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+    setModalOpen(false);
+  };
+
+  return (
+    <Box className={styles.imagenes}>
+      {/* Renderización de imágenes */}
+      {[
+        imagen1, imagen2, imagen3, imagen4, imagen5, imagen6
+      ].map((imageSrc, index) => (
+        <Box key={index} onClick={() => handleImageClick(imageSrc)}>
+          <img
+            src={imageSrc}
+            alt={`imagen ${index + 1}`}
+            loading='lazy'
+          />
+        </Box>
+      ))}
+
+      {/* Modal para mostrar imagen en tamaño completo */}
+      <Modal open={modalOpen} onClose={handleCloseModal}>
+        <div className={styles.modalContent}>
+          {selectedImage && (
             <img
-             src={imagen1}
-             alt='imagen 1'
-             loading='lazy'
+              src={selectedImage}
+              alt='Imagen en tamaño completo'
+              loading='lazy'
+              onClick={handleCloseModal}
             />
-        </Box>
-        <Box>
-        <img
-         src={imagen2}
-         alt='imagen 2'
-         loading='lazy'
-        />
-        </Box>
-        <Box>
-        <img
-         src={imagen3}
-         alt='imagen 3'
-         loading='lazy'
-        />
-        </Box>
-        <Box>
-        <img
-         src={imagen4}
-         alt='imagen 4'
-         loading='lazy'
-        />
-        </Box>
-        <Box>
-        <img
-         src={imagen5}
-         alt='imagen 5'
-         loading='lazy'
-        />
-        </Box>
-        <Box>
-        <img
-         src={imagen6}
-         alt='imagen 6'
-         loading='lazy'
-        />
-        </Box>
-    </Box>)
+          )}
+        </div>
+      </Modal>
+    </Box>
+  );
 }
