a1ed3ad907e45cf8893f557a2d4fdb730adb2824
Ale Candia <alejandrogcandia@gmail.com>
2023-08-17 09:12:13 -0300

    se retoca el css de las imagenes principales de todos los componentes de la pagina

--- src/components/Examenes/Examenes.jsx
+++ src/components/Examenes/Examenes.jsx
@@ -1,6 +1,15 @@
+import { Box } from "@mui/material"
+import imagen from '../../images/Examenes/examenes.jpg'
+import styles from './Examenes.module.css'
 export default function Examenes (){
-    return(<>
-
-<h1>Examenes</h1>
-</>)
+    return(<Box>
+        <Box className={styles.image}>
+        <img
+  src={imagen}
+  alt='background cabecera institucional'
+  loading='lazy'
+  />
+        </Box>
+        <Box>contenido</Box>
+    </Box>)
 }
