dccb1c55e130b8495e29e99064926bd9bf972ef6
Ale Candia <alejandrogcandia@gmail.com>
2023-08-17 10:39:59 -0300

    se retoca css de institucional, y se crea componente Viajes de estudio

--- src/components/ViajesEstudio/ViajesEstudio.jsx
+++ src/components/ViajesEstudio/ViajesEstudio.jsx
@@ -1,6 +1,18 @@
+import { Box } from "@mui/material"
+import imagen from '../../images/Viajes/viaje_estudio.jpg'
+import styles from './ViajesEstudio.module.css'
 export default function ViajesEstudio (){
-    return(<>
+    return(<Box>
 
-<h1>ViajesEstudio</h1>
-</>)
+<Box className={styles.image}>
+<img
+  src={imagen}
+  alt='background cabecera institucional'
+  loading='lazy'
+  />
+</Box>
+<Box></Box>
+<Box></Box>
+
+    </Box>)
 }
