83f217825dfbfde6a0d68811653cd556e9e50fff
Ale Candia <alejandrogcandia@gmail.com>
2023-08-23 11:22:33 -0300

    fix

--- src/components/Index/BoxNiveles/CardNiveles/CardNiveles.jsx
+++ src/components/Index/BoxNiveles/CardNiveles/CardNiveles.jsx
@@ -1,6 +1,6 @@
 import { Box,  Typography } from "@mui/material";
 import kinder from '../../../../images/BoxNiveles/kinder.jpg'
-import primaria from '../../../../images/boxNiveles/primaria.jpg'
+import primaria from '../../../../images/BoxNiveles/primaria.jpg'
 import secundaria from '../../../../images/boxNiveles/secundaria.jpg'
 import styles from './CardNiveles.module.css'
 export default function CardNiveles(){
